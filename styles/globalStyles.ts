a23a359a86e3756593514dc029f578182f8a0469
Valle Ivinskiy <71266129+ivinskiy@users.noreply.github.com>
2022-12-17 21:49:48 +0100

    fix fonts on safari and ios

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -3,22 +3,22 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
    @font-face {
     font-family: DPDorkDiary;
-    src: url('DPDorkDiary.woff2') format('woff2'),
-        url('DPDorkDiary.woff') format('woff');
+    src: url('fonts/DPDorkDiary.woff2') format('woff2'),
+        url('fonts/DPDorkDiary.woff') format('woff');
     font-weight: 500;
     font-style: normal;
     font-display: swap;
     }
     @font-face {
     font-family: Rosario;
-    src: url('Rosario-Regular.ttf') format('ttf');
+    src: url('fonts/Rosario-Regular.ttf') format('truetype');
     font-weight: 400;
     font-style: normal;
     font-display: swap;
     }
     @font-face {
     font-family: Rosario;
-    src: url('Rosario-Bold.ttf') format('ttf');
+    src: url('fonts/Rosario-Bold.ttf') format('truetype');
     font-weight: 700;
     font-style: normal;
     font-display: swap;
